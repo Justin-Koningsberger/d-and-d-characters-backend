da9f88631de98b518a2190d32cbb9c8d4b9d4061
Justin <justin.koningsberger@gmail.com>
2021-10-12 18:12:00 +0200

    Character backend, create and retrieve character

---
+++ index.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const app = express()
+
+const morgan_settings = morgan((tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ')
+})
+
+const mongo_url = process.env.MONGODB_URI
+console.log('connecting to', mongo_url)
+
+mongoose.connect(mongo_url, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+  console.log('connected to MongoDB')
+}).catch(error => {
+  console.log('error connecting to MongoDB:', error.message)
+})
+
+const characterSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 3
+  }
+})
+characterSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+const Character = mongoose.model('Character', characterSchema)
+
+app.use(express.json())
+app.use(express.static('build'))
+app.use(morgan_settings)
+app.use(cors())
+
+app.post('/api/characters', async (req, resp, next) => {
+  const character = new Character({ name: req.body.name })
+
+  try {
+    const savedCharacter = await character.save()
+    resp.status(200).json(savedCharacter)
+  }
+  catch (error) {
+    next(error)
+  }
+})
+
+app.get('/api/characters/:id', async (req, resp, next) => {
+  const id = req.params.id
+
+  try {
+    const character = await Character.findById(id)
+
+    resp.status(200).json(character)
+  }
+  catch (error) {
+    next(error)
+  }
+})
+
+const unknownEndpoint = (req, resp, next) => {
+  console.log(req)
+  resp.status(404).send({ error: 'unknown endpoint' })
+  next()
+}
+
+const errorHandler = (error, req, resp, next) => {
+  console.log(error.message)
+
+  if (error.name === 'CastError') {
+    return resp.status(400).send({ error: 'malformatted id' })
+  }
+  else if ( error.name === 'ValidationError') {
+    return resp.status(400).send({ error: error.message })
+  }
+  next(error)
+}
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
